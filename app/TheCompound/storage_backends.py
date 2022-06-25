d68deb66e9ad9fae913111c7dfc4c19c72e111cc
SethAngell <accounts@sethangell.com>
2022-06-25 15:31:17 -0400

    tweak

--- app/TheCompound/storage_backends.py
+++ app/TheCompound/storage_backends.py
@@ -4,8 +4,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = "static"
+    AWS_DEFAULT_ACL = "public-read"
 
 
 class PublicMediaStorage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+    AWS_DEFAULT_ACL = "public-read"
