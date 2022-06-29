243f3b29237b54450c64c3395ad21892b1700fb6
SethAngell <accounts@sethangell.com>
2022-06-28 21:45:41 -0400

    Added config for tailwind compilation. Running into issues with the background images not compiling. Need to investigate further

---
+++ app/jstoolchain/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    '../templates/*.html',
+    '../accounts/templates/accounts/*.html',
+    '../accounts/templates/registration/*.html',
+    '../blog/templates/blog/*.html',
+    '../landing_page/templates/landing_page/*.html'
+  ],
+  theme: {
+    colors: {
+      'seth-blue': {
+        400: '#05386B',
+        600: '#05056B'
+      },
+      teal: colors.teal,
+      slate: colors.slate,
+
+    },
+    extend: {},
+  },
+  plugins: [],
+
+}
