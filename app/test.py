1e65fda6cbf2533d7a0b25a85fd0886ce1638999
SethAngell <accounts@sethangell.com>
2022-07-07 20:20:39 -0400

    Testing package creation

---
+++ app/test.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+import boto3
+
+linode_obj_config = {
+    "aws_access_key_id": os.environ.get("S3_KEY"),
+    "aws_secret_access_key": os.environ.get("S3_SECRET_KEY"),
+    "endpoint_url": "https://cdn.doublel.studio/",
+}
+
+client = boto3.client("s3", **linode_obj_config)
+
+response = client.list_buckets()
+for bucket in response["Buckets"]:
+    print(bucket["Name"])
+
+time.sleep(10)
