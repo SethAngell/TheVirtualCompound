05ce6d91827fd4c7e11800fd6b3a8741c7b82305
SethAngell <accounts@sethangell.com>
2022-07-07 21:04:30 -0400

    Testing package creation

--- app/test.py
+++ app/test.py
@@ -6,11 +6,9 @@ import boto3
 linode_obj_config = {
     "aws_access_key_id": os.environ.get("S3_KEY"),
     "aws_secret_access_key": os.environ.get("S3_SECRET_KEY"),
-    "endpoint_url": "https://cdn.doublel.studio/",
+    "endpoint_url": "cdn.doublel.studio.us-southeast-1.linodeobjects.com",
 }
 
-for key in linode_obj_config.keys():
-    print(f"{key} : {linode_obj_config[key][:-1]} {linode_obj_config[key][-1]}")
 
 client = boto3.client("s3", **linode_obj_config)
 
